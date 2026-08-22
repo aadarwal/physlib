0ee1297990690f09e4956313ab1a0fbeab620d2b
Aadarsh Agarwal <aadarsh@uchicago.edu>
2026-08-21 23:18:48 -0400

    feat(Optics): bridge Jones and harmonic fields

--- Physlib.lean
+++ Physlib.lean
@@ -166,6 +166,7 @@ public import Physlib.Optics.Basic
 public import Physlib.Optics.Mode.Basic
 public import Physlib.Optics.Polarization.Basic
 public import Physlib.Optics.Polarization.Coherency
+public import Physlib.Optics.Polarization.HarmonicWave
 public import Physlib.Optics.Polarization.JonesCoherency
 public import Physlib.Particles.BeyondTheStandardModel.GeorgiGlashow.Basic
 public import Physlib.Particles.BeyondTheStandardModel.PatiSalam.Basic
