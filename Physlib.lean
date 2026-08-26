3219bce3e88662a6eb997b3310cdc9a9a50c181f
Aadarsh Agarwal <aadarsh@uchicago.edu>
2026-08-26 10:26:04 -0400

    Promote harmonic material-wave bridge

--- Physlib.lean
+++ Physlib.lean
@@ -431,6 +431,7 @@ public import Physlib.Optics.Polarization.Basic
 public import Physlib.Optics.Polarization.Coherency
 public import Physlib.Optics.Polarization.ComplexRealization
 public import Physlib.Optics.Polarization.Frame
+public import Physlib.Optics.Polarization.HarmonicMaterialWave
 public import Physlib.Optics.Polarization.HarmonicWave
 public import Physlib.Optics.Polarization.IncidenceFrame
 public import Physlib.Optics.Polarization.IncidenceFrameRegression
